ef1238d908ba3cce1602948a28bc0d743b9cc9ef
harsh-apcr <harsh2001apcr@gmail.com>
2022-11-14 00:08:17 +0530

    scripting for compiling and running mr-pr-mpi.cpp added

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
-all:
+pr:
 	clang++ mr-pr-cpp.cpp /usr/lib/x86_64-linux-gnu/libboost_system.a /usr/lib/x86_64-linux-gnu/libboost_iostreams.a /usr/lib/x86_64-linux-gnu/libboost_filesystem.a -pthread -o mr-pr-cpp.o
+
+mpi:
+	mpic++ -std=c++11 mr-pr-mpi.cpp -lboost_system -lpthread -lboost_iostreams -lboost_filesystem -o mr-pr-mpi.o
+
 
 checker:
 	clang++ correctness_checker.cpp -o check
--- run.sh
+++ run.sh
@@ -1,27 +1,58 @@
 
-echo "Running pagerank on barabasi-20000.txt dataset"
-./mr-pr-cpp.o data/barabasi-20000.txt -o result/barabasi-20000-pr-cpp.txt
-echo ""
-echo "Running correctness check of the result, which checks if ranks are within 1e-4"
-echo ""
-./check result/barabasi-20000-pr-cpp.txt result/barabasi-20000-pr-p.txt
-echo "---------------------------------------------------------------------------------"
-echo ""
-echo "Running pagerank on erdos-20000.txt dataset"
-./mr-pr-cpp.o data/erdos-20000.txt -o result/erdos-20000-pr-cpp.txt
-echo ""
-echo "Running correctness check of the result, which checks if ranks are within 1e-4"
-echo ""
-./check result/erdos-20000-pr-cpp.txt result/erdos-20000-pr-p.txt
-echo "---------------------------------------------------------------------------------"
-echo ""
-echo "Running pagerank on java.txt dataset"
-./mr-pr-cpp.o data/erdos-100000.txt -o result/erdos-100000-pr-cpp.txt
-echo ""
-echo "Running correctness check of the result, which checks if ranks are within 1e-4"
-echo ""
-./check result/erdos-100000-pr-cpp.txt result/erdos-100000-pr-p.txt
-echo "---------------------------------------------------------------------------------"
-echo ""
+if [ $1 ==  "cpp" ] ;
+then
+    echo "Running pagerank on barabasi-20000.txt dataset"
+    ./mr-pr-cpp.o data/barabasi-20000.txt -o result/barabasi-20000-pr-cpp.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/barabasi-20000-pr-cpp.txt result/barabasi-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-20000.txt dataset"
+    ./mr-pr-cpp.o data/erdos-20000.txt -o result/erdos-20000-pr-cpp.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-20000-pr-cpp.txt result/erdos-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-100000.txt dataset"
+    ./mr-pr-cpp.o data/erdos-100000.txt -o result/erdos-100000-pr-cpp.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-100000-pr-cpp.txt result/erdos-100000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+elif [ $1 == "mpi" ] ;
+then
+    echo "Running pagerank on barabasi-20000.txt dataset"
+    mpirun -np 8 --oversubscribe ./mr-pr-mpi.o data/barabasi-20000.txt -o result/barabasi-20000-pr-mpi.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/barabasi-20000-pr-mpi.txt result/barabasi-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-20000.txt dataset"
+    mpirun -np 8 --oversubscribe ./mr-pr-mpi.o data/erdos-20000.txt -o result/erdos-20000-pr-mpi.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-20000-pr-mpi.txt result/erdos-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-100000.txt dataset"
+    mpirun -np 8 --oversubscribe ./mr-pr-mpi.o data/erdos-100000.txt -o result/erdos-100000-pr-mpi.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-100000-pr-mpi.txt result/erdos-100000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+else
+    echo "invalid input $1 : put one of cpp, mpi, base"
+fi
 
 
