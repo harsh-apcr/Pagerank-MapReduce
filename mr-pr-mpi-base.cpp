3fe9a575325d731d15947f3618c7d1706596d74e
harsh-apcr <harsh2001apcr@gmail.com>
2022-11-14 19:31:46 +0530

    some debugging/optimizations done

--- mr-pr-mpi-base.cpp
+++ mr-pr-mpi-base.cpp
@@ -1,5 +1,4 @@
 #include <cstdio>
-#include <unordered_map>
 #include <fstream>
 #include <string>
 #include <vector>
@@ -14,9 +13,13 @@
 #include "mapreduce-7Apr14/src/keyvalue.h"
 
 using namespace MAPREDUCE_NS;
-std::unordered_map<std::uint32_t, std::uint32_t> num_outgoing;
-std::vector<std::vector<std::uint32_t>> incoming;
 
+std::unordered_map<std::uint32_t, std::uint32_t> num_outgoing;
+std::vector<std::pair<std::uint32_t, std::uint32_t>> hyperlink;
+std::uint32_t **incoming;
+std::uint32_t *incoming_nval;
+
+int hlinksize;
 int websize;
 const double DEFAULT_ALPHA = 0.85;
 const double DEFAULT_CONVERGENCE = 0.00001;
@@ -36,15 +39,14 @@ std::vector<std::string> split(const std::string &s, char seperator) {
 
 
 std::vector<double>
-run_pgrank(std::vector<std::vector<std::uint32_t>> incoming,
-        std::unordered_map<std::uint32_t, std::uint32_t> num_outgoing,
+run_pgrank(std::unordered_map<std::uint32_t, std::uint32_t> num_outgoing,
         double convergence,
         int max_iterations,
         double alpha) {
 
     // incoming[i] = vector of all pg ids that have a link to pg i
     // num_outgoing[i] = number of outgoing links out of pg i
-    int n = incoming.size();    // websize
+    int n = websize;
     double sum_pr;
     double dangling_pr;
     double diff = 1;
@@ -91,9 +93,10 @@ run_pgrank(std::vector<std::vector<std::uint32_t>> incoming,
         for(unsigned i = 0;i < n;i++) {
             /* The corresponding element of the H multiplication */
             double h = 0.0;
-            for(int pg : incoming[i]) {
+            unsigned sz = incoming_nval[i];
+            for(unsigned j = 0;j < sz;j++) {
+                std::uint32_t pg = incoming[i][j];
                 // pg -> i
-                assert(num_outgoing[pg]);   // TODO: remove this after testing
                 h += 1.0 / num_outgoing[pg] * old_pr[pg];
             }
             h *= alpha;
@@ -107,49 +110,31 @@ run_pgrank(std::vector<std::vector<std::uint32_t>> incoming,
     return pr;
 }
 
-void fileread(int /*itask*/, char *fname, KeyValue *kv, void */*ptr*/) {
-    std::filebuf fb1;
-    if (fb1.open(fname, std::ios::in)) {
-        std::istream input_stream(&fb1);
-        std::string line;
-        std::regex p("(0|[1-9][0-9]*)\\s(0|[1-9][0-9]*)$");
-        unsigned int i = 0;
-        websize = 0;
-        while (std::getline(input_stream, line)) {
-            if (!std::regex_match(line, p)) {
-                fprintf(stderr, "invalid input at line number : %d", i+1);
-                MPI_Abort(MPI_COMM_WORLD,1);
-            }
-            // regex match
-            std::vector<std::string> link = split(line, ' ');
-            int key = std::stoi(link[1]);
-            int val = std::stoi(link[0]);
-            kv->add((char*)&key, sizeof(std::uint32_t), (char*)&val, sizeof(std::uint32_t));
-            num_outgoing[val]++;
-            
-            if (websize < key)
-                websize = key;
-            if (websize < val)
-                websize = val;
-            i++;
-        }
-        websize++;
-    } else {
-        std::cerr << "cannot open file `" << fname << "`" << std::endl;
+void fileread(int rank, KeyValue *kv, void *ptr) {
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    for (int i = rank * (hlinksize / size); i < (rank+1) * (hlinksize / size); i++) {
+        kv->add((char *) &hyperlink[i].second,sizeof(std::uint32_t),    // key
+                (char *) &hyperlink[i].first,sizeof(std::uint32_t));    // val
     }
 }
 
-void collect_incoming(char *key, int keybytes, char *multivalue,
+void collect_incoming(uint64_t /*itask*/, char *key, int keybytes, 
+    char *value, int valuebytes, KeyValue *kv, void * ptr) {
+
+    int rank = *reinterpret_cast<int*>(ptr);
+    if (rank != 0) return;
+    
+    std::uint32_t k = *reinterpret_cast<std::uint32_t*>(key);
+    incoming[k] = reinterpret_cast<std::uint32_t*>(value);
+    incoming_nval[k] = valuebytes / sizeof(std::uint32_t);
+    kv->add(key, keybytes, value, valuebytes);
+}
+
+void collect(char *key, int keybytes, char *multivalue,
  int nvalues, int *valuebytes, KeyValue *kv, void *ptr) {
-    uint *values = reinterpret_cast<uint*>(multivalue);
-    int k = *reinterpret_cast<uint*>(key);
-    for(unsigned i = 0;i < nvalues;i++) {
-        incoming[k].push_back(values[i]);
-    }
     kv->add(key, keybytes, multivalue, sizeof(std::uint32_t) * nvalues);
 }
-
-
 
 int main(int argc, char **argv) {
     MPI_Init(&argc, &argv);
@@ -168,25 +153,69 @@ int main(int argc, char **argv) {
         MPI_Abort(MPI_COMM_WORLD,1);
     }
 
-    MapReduce *mr = new MapReduce(MPI_COMM_WORLD);
-    char *files[2] = {argv[1], NULL};
+    std::filebuf fb1;
+    if (fb1.open(argv[1], std::ios::in)) {
+        std::istream input_stream(&fb1);
+        std::string line;
+        std::regex p("(0|[1-9][0-9]*)\\s(0|[1-9][0-9]*)$");
+        unsigned int i = 0;
+        websize = 0;
+        while (std::getline(input_stream, line)) {
+            if (!std::regex_match(line, p)) {
+                fprintf(stderr, "invalid input at line number : %d", i+1);
+                MPI_Abort(MPI_COMM_WORLD,1);
+            }
+            // regex match
+            std::vector<std::string> link = split(line, ' ');
+            int pg1 = std::stoi(link[0]);
+            int pg2 = std::stoi(link[1]);
+            hyperlink.push_back(std::make_pair(pg1, pg2));
+            num_outgoing[pg1]++;
+            
+            if (websize < pg1)
+                websize = pg1;
+            if (websize < pg2)
+                websize = pg2;
+            i++;
+        }
+        websize++;
+    } else {
+        std::cerr << "cannot open file `" << argv[1] << "`" << std::endl;
+    }
 
+    hlinksize = hyperlink.size();
+    MapReduce *mr = new MapReduce(MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
     double start = MPI_Wtime();
-    int hlinksize = mr->map(size, files, 0, 1, 0, fileread, NULL);
-    mr->gather(1);
-    if (rank == 0) {
-        mr->collate(NULL);                                                // reduce
-        incoming.resize(websize);
-        mr->reduce(collect_incoming, NULL);                 // no need for reduce, collate does the equivalent job
-        
-        double stop = MPI_Wtime();
-        std::cout << "\nBaseMPI-MapReduce job finished in " << (stop - start) << " s" << std::endl;
 
+    mr->map(size, fileread, NULL);
+    mr->collate(NULL);              // kv -> kmv
+    mr->reduce(collect, NULL);
+
+    MPI_Barrier(MPI_COMM_WORLD);
+    double stop = MPI_Wtime();
+    if (rank == 0)
+        std::cout << "\nBaseMPI-MapReduce job finished in " << (stop - start) << " s" << std::endl;
+    
+    mr->gather(1);
+
+    if (rank == 0) {
+        incoming = new std::uint32_t*[websize];
+        memset(incoming, 0, sizeof(std::uint32_t) * websize);
+        incoming_nval = new std::uint32_t[websize];
+        memset(incoming_nval, 0, sizeof(std::uint32_t) * websize);
+    }
+
+    mr->map(mr, collect_incoming, &rank);
+
+    if (rank == 0) {
         auto pgstart = std::chrono::high_resolution_clock::now();
-        auto pgrankv = run_pgrank(incoming, num_outgoing, DEFAULT_CONVERGENCE, DEFAULT_MAX_ITERATIONS, DEFAULT_ALPHA);
+        auto pgrankv = run_pgrank(num_outgoing, DEFAULT_CONVERGENCE, DEFAULT_MAX_ITERATIONS, DEFAULT_ALPHA);
         auto pgend = std::chrono::high_resolution_clock::now();
 
+        delete mr;
+        delete [] incoming;
+        delete [] incoming_nval;
         auto duration = std::chrono::duration_cast<std::chrono::microseconds>(pgend - pgstart);
         std::cout << "\nPagerank algorithm finished in " << duration.count() << "us" << std::endl;
 
@@ -206,7 +235,5 @@ int main(int argc, char **argv) {
         }
 
     }
-    
     MPI_Finalize();
-
 }
