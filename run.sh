cbeec4854c6d795f8a95bd6c3f6cf91dc6d9d49e
harsh-apcr <harsh2001apcr@gmail.com>
2022-11-14 23:49:14 +0530

    run script updated

--- run.sh
+++ run.sh
@@ -51,6 +51,32 @@ then
     ./check result/erdos-100000-pr-mpi.txt result/erdos-100000-pr-p.txt
     echo "---------------------------------------------------------------------------------"
     echo ""
+elif [ $1 == "base" ];
+then
+    echo "Running pagerank on barabasi-20000.txt dataset"
+    ./mr-pr-mpi-base.o data/barabasi-20000.txt -o result/barabasi-20000-pr-mpi-base.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/barabasi-20000-pr-mpi.txt result/barabasi-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-20000.txt dataset"
+    ./mr-pr-mpi-base.o data/erdos-20000.txt -o result/erdos-20000-pr-mpi-base.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-20000-pr-mpi-base.txt result/erdos-20000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
+    echo "Running pagerank on erdos-100000.txt dataset"
+    ./mr-pr-mpi-base.o data/erdos-100000.txt -o result/erdos-100000-pr-mpi.txt
+    echo ""
+    echo "Running correctness check of the result, which checks if ranks are within 1e-4"
+    echo ""
+    ./check result/erdos-100000-pr-mpi.txt result/erdos-100000-pr-p.txt
+    echo "---------------------------------------------------------------------------------"
+    echo ""
 else
     echo "invalid input $1 : put one of cpp, mpi, base"
 fi
